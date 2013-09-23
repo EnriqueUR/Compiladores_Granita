6c9b1a40ca8161d860b61a2fef72b85e8748316e
Enrique Urbina <eur_it07@hotmail.es>
2013-09-22 18:54:58 -0600

    statement

--- Granita/statement.h
+++ Granita/statement.h
@@ -10,13 +10,9 @@ public:
     statement();
     virtual void print() = 0;
     int Linea;
-<<<<<<< HEAD
-    virtual newStatement *validarSemantica();
-    virtual PrintError(string msj);
-=======
+
     virtual newStatement *ValidarSemantica();
     virtual void PrintError(string msj);
->>>>>>> f120c4fc22361d1debd8011e664e906ef9d2f3c8
 };
 
 #endif // STATEMENT_H
